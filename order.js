d7b5292e17aae25f7fd6bcff7999fb9ada814b27
Vikasha_das <80262021+vikashadas@users.noreply.github.com>
2023-09-28 23:54:16 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("#header-container").load("header.html");
+  });
+  let count = $("#count");
+//getting values from local storage using json.parse
+let value = JSON.parse(localStorage.getItem("Products"));
+
+value.splice(0);
+localStorage.setItem("Products", JSON.stringify(value));
+value = JSON.parse(localStorage.getItem("Products"));
+
+//displaying number count for items added in cart
+if(value==""||value==null){
+    count.text("0")
+}
+$("#add2cart").click(function(){
+    location.assign("./index.html");
+})
+$(document).ready(function() {
+    $("#footer-container").load("footer.html");
+});
